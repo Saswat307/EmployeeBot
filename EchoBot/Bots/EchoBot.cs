badfaa7d6b0d4d2a3f01eea280742f1d18c6c92b
SASWATA PANIGRAHI <saspanigrahi@microsoft.com>
2024-09-13 11:44:51 +0530

    first commit

---
+++ EchoBot/Bots/EchoBot.cs
@@ -0,0 +1,41 @@
+﻿// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.22.0
+
+using EchoBot.Services;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        private SpeechService _speechService;
+        
+        public EchoBot(SpeechService speechService)
+        {
+            _speechService = speechService;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var replyText = $"Echo: {turnContext.Activity.Text}";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            //await _speechService.ConvertTextToSpeechAsync(replyText, "output.wav");
+            //await _botService.SendAudioToTeamsMeetingAsync("output.wav");
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ EchoBot/Services/SpeechService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+using System.Threading.Tasks;
+using System;
+
+namespace EchoBot.Services
+{
+    public class SpeechService
+    {
+
+        private string SpeechSubscriptionKey = "";
+        private string SpeechRegion = "eastus";
+
+        private SpeechConfig _speechConfig;
+        
+        public SpeechService()
+        {
+            //_speechConfig = SpeechConfig.FromSubscription(SpeechSubscriptionKey, SpeechRegion);
+        }
+        public async Task ConvertTextToSpeechAsync(string text, string outputFile)
+        {
+            var _audioConfig = AudioConfig.FromWavFileOutput(outputFile);
+            using (var synthesizer = new SpeechSynthesizer(_speechConfig, _audioConfig))
+            {
+                var result = await synthesizer.SpeakTextAsync(text);
+
+                if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                {
+                    Console.WriteLine($"Speech synthesis passed: {result}");
+                    // You can now send this "output.wav" to Teams meeting using the Calling SDK
+                    //await SendAudioToTeamsMeetingAsync("output.wav");
+                }
+                else
+                {
+                    Console.WriteLine($"Speech synthesis failed: {result.Reason}");
+                }
+            }
+        }
+    }
+}
